aa923c220488ff7605d21330f6b20944f58017cd
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2022-03-01 17:46:47 +0900

    Add files via upload

--- models/networks/__init__.py
+++ models/networks/__init__.py
@@ -8,6 +8,7 @@ from models.networks.base_network import BaseNetwork
 from models.networks.loss import *
 from models.networks.discriminator import *
 from models.networks.generator import *
+from models.networks.generator_point import *
 from models.networks.encoder import *
 import util.util as util
 
