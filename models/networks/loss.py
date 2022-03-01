8c46b82da82530963957d110650de19c76764447
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2022-03-01 17:45:44 +0900

    Add files via upload

--- models/networks/loss.py
+++ models/networks/loss.py
@@ -118,3 +118,28 @@ class VGGLoss(nn.Module):
 class KLDLoss(nn.Module):
     def forward(self, mu, logvar):
         return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+
+class ChamferLoss(nn.Module):
+    def __init__(self):
+        super(ChamferLoss, self).__init__()
+        self.use_cuda = torch.cuda.is_available()
+
+    def batch_pairwise_dist(self, x, y):
+        bs, num_points_x, points_dim = x.size()
+        _, num_points_y, _ = y.size()
+        xx = x.pow(2).sum(dim=-1)
+        yy = y.pow(2).sum(dim=-1)
+        zz = torch.bmm(x, y.transpose(2, 1))
+        rx = xx.unsqueeze(1).expand_as(zz.transpose(2, 1))
+        ry = yy.unsqueeze(1).expand_as(zz)
+        P = (rx.transpose(2, 1) + ry - 2 * zz)
+        return P
+
+    def forward(self, preds, gts):
+        P = self.batch_pairwise_dist(gts, preds)
+        mins, _ = torch.min(P, 1)
+        loss_1 = torch.sum(mins)
+        mins, _ = torch.min(P, 2)
+        loss_2 = torch.sum(mins)
+        return loss_1 + loss_2
